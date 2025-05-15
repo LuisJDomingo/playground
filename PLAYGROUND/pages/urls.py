4a90ada3bfb00e3524821bf91db9ae8be6c60298
LuisJDomingo <luisdomingogarcia79@gmail.com>
2025-05-15 18:31:26 +0200

    create view

--- PLAYGROUND/pages/urls.py
+++ PLAYGROUND/pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
-from .views import PageListView, PageDetailview
+from .views import PageListView, PageDetailview, PageCreateView
 
-urlpatterns = [
-    path('', PageListView.as_view(), name='pages'),
-    path('<int:pk>/<slug:page_slug>/', PageDetailview.as_view(),  name='page'),
-]
+pages_patterns =( 
+    [
+        path('', PageListView.as_view(), name='pages'),
+        path('<int:pk>/<slug:page_slug>/', PageDetailview.as_view(),  name='page'),
+        path('create/', PageCreateView.as_view(), name = 'create'),]
+        
+        , 'pages')
