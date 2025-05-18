c89cc486163ef6c4184a7e9a627f0db6eeeae108
LuisJDomingo <luisdomingogarcia79@gmail.com>
2025-05-18 11:46:55 +0200

    recuperacion contraseña

--- PLAYGROUND/PLAYGROUND/settings.py
+++ PLAYGROUND/PLAYGROUND/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+#import os
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,8 +137,8 @@ LOGOUT_REDIRECT_URL = 'home'
 # Emails
 
 if DEBUG:
-    EMAIL_BACKEND = "django.core.mail.backends.filebased.Emailbackend"
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
     EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 else:
-    # Aqui hay que configurar un mail real para prodduccion
+    # Aqui hay que configurar un mail real para produccion
     pass
