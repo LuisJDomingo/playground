3f5474bcb3002193812914854660532675ef8530
LuisJDomingo <luisdomingogarcia79@gmail.com>
2025-05-20 09:29:35 +0200

    change mail user

--- PLAYGROUND/registration/forms.py
+++ PLAYGROUND/registration/forms.py
@@ -25,3 +25,17 @@ class ProfileForm(forms.ModelForm):
             "bio": forms.Textarea(attrs={"class": "form-control mt-3", "rows": 3, "placeholder": "Escribe algo sobre ti"}),
             "link": forms.URLInput(attrs={"class": "form-control mt3", 'placeholder': "link"}),
         }
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido, 254 caracteres como áximo y debe ser valido")
+
+    class Meta:
+        model = User
+        fields = ["email"]
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("el Email ya existe, prueba con otro")
+        return email
